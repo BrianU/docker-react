c9ed4583321555dcc9c35d7528873ce745dd3ab2
Brian Uniacke <brianuniacke@gmail.com>
2019-10-15 14:31:50 +0100

    Changed the tag line

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Brian you Good thing
+          Learn React be serious
         </a>
       </header>
     </div>
